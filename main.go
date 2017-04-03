5150278d8dd5591198ae94916c7056795381ebc5
Andrew Mu <muandrew@gmail.com>
2017-04-02 17:18:54 -0700

    Display some pii

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 
 	"github.com/labstack/echo"
 	"github.com/muandrew/battlecode-ladder/oauth"
+	"io/ioutil"
+	"fmt"
+	"golang.org/x/oauth2"
 )
 
 var authProviders map[string]*oauth.OAConfig
@@ -33,5 +36,19 @@ func getLogin(c echo.Context) error {
 
 func getAuthorize(c echo.Context) error {
 	app := c.Param("app")
-	return c.Redirect(http.StatusTemporaryRedirect, "https://www." + app + ".com")
+	config := authProviders[app]
+	token, err := config.Config.Exchange(oauth2.NoContext, c.FormValue("code"))
+	if (err == nil) {
+		response, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + token.AccessToken)
+		if (err == nil) {
+			defer response.Body.Close()
+			contents, _ := ioutil.ReadAll(response.Body)
+			return c.String(http.StatusOK, fmt.Sprintf("Content: %s\n", contents))
+		} else {
+			return c.String(http.StatusInternalServerError, "Some error has occured2.")
+		}
+	} else {
+		return c.String(http.StatusInternalServerError, "Some error has occured.")
+	}
+	//return c.Redirect(http.StatusTemporaryRedirect, "https://www." + app + ".com")
 }

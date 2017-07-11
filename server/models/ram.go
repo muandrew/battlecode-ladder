79da101c60b212d05da32c8006b3767727420184
Andrew Mu <muandrew@gmail.com>
2017-07-10 23:20:30 -0700

    Add Resource Access and Management
     * access are based on tiers, no ability to select read or write only.
     * potentially use masking bits in the future.

---
+++ server/models/ram.go
@@ -0,0 +1,70 @@
+package models
+
+import "github.com/muandrew/battlecode-ladder/utils"
+
+/**
+Resource Access and Management
+
+Currently only the creator has write access.
+ */
+type RAM struct {
+	owner      *Competitor
+	readPublic bool
+	whitelist  map[Competitor]ramAccessTier
+}
+
+type ramAccessTier int
+
+const (
+	RamAccessNone = ramAccessTier(0)
+	RamAccessR    = ramAccessTier(1)
+	RamAccessRW   = ramAccessTier(2)
+	accessError   = utils.Error("Insufficient permission.")
+)
+
+func CreateRAM(owner *Competitor) *RAM {
+	return &RAM{owner: owner}
+}
+
+func (r *RAM) ReadAllowed(competitor *Competitor) bool {
+	if r.readPublic {
+		return true
+	}
+	return r.userBasedAccess(competitor, RamAccessR)
+}
+
+func (r *RAM) WriteAllowed(competitor *Competitor) bool {
+	return r.userBasedAccess(competitor, RamAccessRW)
+}
+
+func (r *RAM) SetPublic(actor *Competitor, public bool) error {
+	if !r.WriteAllowed(actor) {
+		return accessError
+	}
+	r.readPublic = public
+	return nil
+}
+
+func (r *RAM) SetAccess(actor *Competitor, competitor *Competitor, access ramAccessTier) error {
+	if !r.WriteAllowed(actor) {
+		return accessError
+	}
+	if access == RamAccessNone {
+		delete(r.whitelist, competitor.AsValue())
+	} else {
+		r.whitelist[competitor.AsValue()] = access
+	}
+	return nil
+}
+
+func (r *RAM) userBasedAccess(competitor *Competitor, access ramAccessTier) bool {
+	if competitor == nil {
+		return false
+	} else if competitor.Equals(r.owner) {
+		return true
+	} else if r.whitelist[competitor.AsValue()] >= access {
+		return true
+	} else {
+		return false
+	}
+}
